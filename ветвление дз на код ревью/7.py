09dc34b992a74ae5e20052026f76c65bdd8fe254
pEngwin <137907908+1pEngwin1@users.noreply.github.com>
2023-07-07 18:29:34 +0300

    Update 7.py

--- ветвление дз на код ревью/7.py
+++ ветвление дз на код ревью/7.py
@@ -10,13 +10,13 @@ if a < d:
     x = b - a + 1
     y = d - c + 1
     if c > a and b < d:
-        dlina = max(x,y) - abs(a - c)
+        dlina = max(x,y) - abs(d - b)
     elif c > a and b > d:
         dlina = max(x, y) - abs(a - c) - abs(b - d)
     elif a > c and b < d:
         dlina = max(x, y) - abs(a - c) - abs(b - d)
     elif a > c and b > d:
-        dlina = max(x, y) - abs(a - c)
+        dlina = max(x, y) - abs(b - d)
 elif a == d or c == b:
     dlina = 1
 
